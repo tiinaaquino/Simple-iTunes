6feebe07327a22d81106f6a473f8b34bb8b7539b
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-05-16 22:48:54 +0000

    committing

---
+++ cs112/project4 - FINAL/src/project4FINAL/Driver.java
@@ -0,0 +1,19 @@
+package project4FINAL;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Driver {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Mp3 player");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SongDatabase songs = new SongDatabase();
+		MPlayerPanel panel = new MPlayerPanel(songs);
+		panel.setPreferredSize(new Dimension(600,400));
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
