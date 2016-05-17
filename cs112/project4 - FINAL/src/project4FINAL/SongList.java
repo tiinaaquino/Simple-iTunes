622e1f8a0cd70e7f617cb4b9d30b01801e4b411c
cbaquino <cbaquino@ebce0d44-4c41-4e8f-97e2-159a21da1814>
2016-05-17 03:20:39 +0000

    new methods

--- cs112/project4 - FINAL/src/project4FINAL/SongList.java
+++ cs112/project4 - FINAL/src/project4FINAL/SongList.java
@@ -1,28 +1,54 @@
 package project4FINAL;
 
-//import lab7.Link;
+import java.util.Iterator;
 
-public class SongList {
+public class SongList implements Iterable<Song>{
 	private SongNode head;
 	private int size;
 
 	public SongList() {
-		head = null;
 		size = 0;
 	}
 
 	// insert
-	public void insert(Song newSong) {
+	public void addSong(Song newSong) {
 		if (head == null) {
 			head = new SongNode(newSong);
 		} else {
-			insertHelper(newSong);
+			addSongHelper(newSong);
 		}
 		size++;
 	}
+	
+	public Song getSong(int index) {
+		Song song = null;
+		SongNode current = head;
+		int count = 0;
+		while (count != index) {
+			current = current.next();
+			count++;
+		}
+		// song = current.song;
+		song = current.elem();
+		return song;
+	}
+	
+	public SongList search(String title) {
+		SongList songs = new SongList();
+		SongNode curr = head;
+		while (curr != null) {
+			// Song song = curr.song;
+			Song song = curr.elem();
+			if (song.getTitle().startsWith(title)) {
+				songs.addSong(song);
+			}
+			curr = curr.next();
+		}
+		return songs;
+	}
 
-	// insert alaphabetically
-	private void insertHelper(Song aSong) {
+	// insert alphabetically
+	private void addSongHelper(Song aSong) {
 		SongNode parent = null;
 		SongNode child = head;
 		while (child != null && child.elem().compareTo(aSong) < 0) {
@@ -44,14 +70,21 @@ public class SongList {
 			}
 		}
 	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		SongNode currentSong = head;
+		while (currentSong != null) {
+			sb.append(currentSong.elem().toString() + "\n");
+			currentSong = currentSong.next();
+		}
+		return sb.toString();
+	}
 
 	public int size() {
 		return size;
 	}
 
-	// find
-
-	// print songs
 	public void printSongs() {
 		SongNode curr = head;
 		while (curr.next() != null) {
@@ -60,5 +93,43 @@ public class SongList {
 		}
 		System.out.println();
 	}
+	
+	/*
+	private class SongNode { 
+		SongNode next;
+		Song song;
+		
+		public SongNode(Song song) {
+			this.song = song;
+		}
+	}
+	*/
+	
+	 @Override
+	 public Iterator<Song> iterator() {
+		 return new InOrder(this);
+	 }
+	 
+	 private class InOrder implements Iterator<Song> {
+		 private SongList list;
+		 private int count;
+		 
+		 public InOrder(SongList list) {
+			 count = 0;
+			 this.list = list;
+		 }
+		 
+		 @Override
+		 public boolean hasNext() {
+			 return count != list.size();
+		 }
+		 
+		 @Override
+		 public Song next() {
+			 Song song = list.getSong(count);
+			 count++;
+			 return song;
+		 }
+	 }
 
 }
